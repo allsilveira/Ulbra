71d9df89a656bc15ae973c691838ed19ef1a662f
allsilveira <silveiraschutt@gmail.com>
2022-11-24 19:50:46 -0300

    automação com Selenium

---
+++ 2022_02/engenharia_de_software/aula12/automacao-selenium.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# passo 1
+driver = webdriver.Chrome()
+driver.get('https://www.google.com')
+driver.maximize_window()
+search_box = driver.find_element(By.CSS_SELECTOR, ('.gLFyf'))
+search_box.send_keys('Computação ULBRA Torres')
+search_box.submit()
+driver.quit()
+
+# passo 2
+import time
+browser = webdriver.Chrome()
+browser.get("https://www.browserstack.com/users/sign_in") 
+time.sleep(10)
+user = browser.find_element_by_id("user_email_login")
+password = browser.find_element_by_id("user_password")
+user.send_keys("alesssandra.s@rede.ulbra.br")
+password.send_keys("colorido")
+login_attempt = browser.find_element_by_xpath("//*[@type='submit']")
+login_attempt.submit()
+browser.quit()
